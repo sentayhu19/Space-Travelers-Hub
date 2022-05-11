c195bcc439a3292909f42d1cefada14fb03c8960
Theophileaseh <theophilenwachan@gmail.com>
2022-05-11 14:01:07 +0100

    Fetch rockets

--- src/redux/rockets/action.js
+++ src/redux/rockets/action.js
@@ -1,7 +1,36 @@
-const ROCKETS = 'FETCH_MISSIONS';
+import axios from 'axios';
 
-const fetchRockets = () => ({
-  type: ROCKETS,
+// Action types
+export const FETCH_ROCKETS_REQUEST = 'FETCH_ROCKETS_REQUEST';
+export const FETCH_ROCKETS_SUCCESS = 'FETCH_ROCKETS_SUCCESS';
+export const FETCH_ROCKETS_FAILURE = 'FETCH_ROCKETS_FAILURE';
+
+// Action Creators
+const fetchRocketsRequest = () => ({
+  type: FETCH_ROCKETS_REQUEST,
 });
+
+const fetchRocketsSuccess = (rockets) => ({
+  type: FETCH_ROCKETS_SUCCESS,
+  payload: rockets,
+});
+
+const fetchRocketsFailure = (error) => ({
+  type: FETCH_ROCKETS_FAILURE,
+  payload: error,
+});
+
+export const fetchRockets = () => (dispatch) => {
+  dispatch(fetchRocketsRequest);
+  axios.get('https://api.spacexdata.com/v3/rockets')
+    .then((response) => {
+      const rockets = response.data;
+      dispatch(fetchRocketsSuccess(rockets));
+    })
+    .catch((error) => {
+      const errorMsg = error.message;
+      dispatch(fetchRocketsFailure(errorMsg));
+    });
+};
 
 export default fetchRockets;
--- src/redux/rockets/reducers.js
+++ src/redux/rockets/reducers.js
@@ -1,8 +1,27 @@
+import * as actions from './action';
+
 const initState = {
   rockets: [],
 };
 const rocketReducer = (state = initState, action) => {
   switch (action.type) {
+    case actions.FETCH_ROCKETS_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case actions.FETCH_ROCKETS_SUCCESS:
+      return {
+        loading: false,
+        rockets: action.payload,
+        error: '',
+      };
+    case actions.FETCH_ROCKETS_FAILURE:
+      return {
+        loading: false,
+        rockets: [],
+        error: action.payload,
+      };
     default:
       return state;
   }
