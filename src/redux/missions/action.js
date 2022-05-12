ffdb4d740187a48338dc42003b950267864bf66d
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-05-13 00:27:55 +0300

    join-and-leave

--- src/redux/missions/action.js
+++ src/redux/missions/action.js
@@ -1,5 +1,8 @@
 export const FETCHMISSION = 'FETCHMISSIONS';
 export const FETCHERROR = 'FETCHERROR';
+export const JOINMISSION = 'JOINMISSION';
+export const LEAVEMISSION = 'LEAVEMISSION';
+
 const axios = require('axios');
 
 export const Url = axios.create({ baseURL: 'https://api.spacexdata.com/v3/missions' });
@@ -11,6 +14,15 @@ export const fetchMission = (mission) => ({
 
 export const fetchError = () => ({
   type: FETCHERROR,
+});
+export const JoinMission = (payload) => ({
+  type: JOINMISSION,
+  payload,
+});
+
+export const leaveMission = (payload) => ({
+  type: LEAVEMISSION,
+  payload,
 });
 
 export const getMission = () => (dispatch) => {
