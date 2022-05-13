ffedc80a0642fd244d58d9b453e9e4852f26bb0b
Theophileaseh <theophilenwachan@gmail.com>
2022-05-13 20:32:03 +0100

    add files

--- src/components/Rockets/rockets.js
+++ src/components/Rockets/rockets.js
@@ -1,4 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Rocket from './Rocket';
+import { getRocketList } from '../../redux/rockets/reducers';
+import './Rockets.css';
 
-const Rockets = () => <h2>Rockets</h2>;
+const Rockets = () => {
+  const dispatchAction = useDispatch();
+  const rockets = useSelector((state) => state.rocketsReducer);
+
+  useEffect(() => {
+    dispatchAction(getRocketList());
+  }, [dispatchAction]);
+
+  return (
+    <div className="rockets-list">
+      { rockets.map((rocket) => (
+        <Rocket
+          key={rocket.id}
+          id={rocket.id}
+          name={rocket.rocket_name}
+          img={rocket.flickr_images[0]}
+          desc={rocket.description}
+        />
+      )) }
+    </div>
+  );
+};
+
 export default Rockets;
